fed59b77da9d4ffb3f5dd091384b30f2d9f6072a
daniel <daniel.schramm@gmx.de>
2024-09-15 20:26:05 +0200

    Change LDAP library for easier installation.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # python-ldap-opnsense
 Script to push host data (DHCP, DNS) from Ldap to OpnSense via API.
+
+Package dependencies - versions are just for info for a known working setup:
+* python 3.12
+* pyopnsense 0.4.0
+* macaddress 2.0.2
+  * Not django-macaddress
+* ldap3 2.9.1
+  * python-ldap is not reliable to install on windows or exotic targets.
--- ldap_opnsense.py
+++ ldap_opnsense.py
@@ -1,10 +1,12 @@
 #!/bin/env python3
 import json
 import sys
+from unittest.mock import DEFAULT
+
 import macaddress
 from pprint import pprint
 
-import ldap
+from ldap3 import Server, Connection, SAFE_SYNC, ALL, SUBTREE
 
 from neighbor_settings_controller_client import NeighborSettingsControllerClient
 
@@ -17,25 +19,41 @@ class MyLdapServer:
 
     def __init__(self, config_data):
         self.conf=config_data
-        self.ldapServer = ldap.initialize(self.conf['ldap_url'])
-        self.searchResult = self.ldapServer.search_s(self.conf['ldap_start'], ldap.SCOPE_SUBTREE,self.conf['ldap_search']
-                                                , ['dhcpHost', 'mail'])
+        server = Server(self.conf['ldap_url'])
+        #self.ldapServer = ldap.initialize(self.conf['ldap_url'])
+        #self.searchResult = self.ldapServer.search_s(self.conf['ldap_start'], ldap.SCOPE_SUBTREE,self.conf['ldap_search']
+        #                                        , ['dhcpHost', 'mail'])
 
-    def refresh_data(self):
-        self.searchResult = self.ldapServer.search_s(self.conf['ldap_start'], ldap.SCOPE_SUBTREE,self.conf['ldap_search']
-                                                , ['dhcpHost', 'mail'])
+        conn = Connection(server, self.conf['ldap_user'], self.conf['ldap_passwd'])
+        #conn = Connection(server)
+        conn.bind()
+        #status, result, response, _ = conn.search(self.conf['ldap_start'], self.conf['ldap_search'])
+        result = conn.search(self.conf['ldap_start'], self.conf['ldap_search'],search_scope=SUBTREE, attributes=['cn','dhcpHWAddress','dhcpStatements','associatedDomain'])
+        #pprint (conn.response)
+        self.searchResult=conn.response
+
+    #def refresh_data(self):
+        #self.searchResult = self.ldapServer.search_s(self.conf['ldap_start'], ldap.SCOPE_SUBTREE,self.conf['ldap_search']
+        #                                        , ['dhcpHost', 'mail'])
 
 
     def get_arp_list_ldap(self):
-        print(len(self.searchResult))
+        print("Host entires found: " + str(len(self.searchResult)))
         arp_list = []
         for hostEntry in self.searchResult:
+            hostname='reading failed'
             try:
-                mac= macaddress.MAC(hostEntry[1]['dhcpHWAddress'][0].decode("utf-8").split()[1])
+                hostname= hostEntry['attributes']['cn'][0]
+                mac= macaddress.MAC(hostEntry['attributes']['dhcpHWAddress'].decode("utf-8").split()[1])
                 arp_list.append(mac)
             except:
-                print("Wrong format for entry on ldap server:")
-                pprint(hostEntry)
+                try:
+                    if hostEntry['attributes']['dhcpHWAddress'] == []:
+                        print("Empty HW address:" + hostname)
+                    else:
+                        print("Wrong format for entry on ldap server: " + hostname)
+                except:
+                    print("Wrong format for entry on ldap server: " + hostname)
 
         return arp_list
 
@@ -44,13 +62,13 @@ class MyLdapServer:
         result={}
         for hostEntry in self.searchResult:
             try:
-                entry_mac = macaddress.MAC(hostEntry[1]['dhcpHWAddress'][0].decode("utf-8").split()[1])
+                entry_mac = macaddress.MAC(hostEntry['attributes']['dhcpHWAddress'].decode("utf-8").split()[1])
                 if mac == entry_mac:
                     print("Mac address match")
-                    #pprint(hostEntry[1])
+                    pprint(hostEntry['attributes'])
                     result['etheraddr']=mac_to_string(mac)
-                    result['ipaddress']=hostEntry[1]['dhcpStatements'][0].decode("utf-8").split()[1]
-                    result['descr']=hostEntry[1]['cn'][0].decode("utf-8")
+                    result['ipaddress']=hostEntry['attributes']['dhcpStatements'][0].split()[1]
+                    result['descr']=hostEntry['attributes']['cn'][0]
                     neighbor['neighbor']=result
                     pprint(neighbor)
             except:
